6fa358cc1b9b18dc74797ee74e2c46d6606f022e
Shu Yamashita <yamashita.s.al@m.titech.ac.jp>
2024-11-03 13:47:43 +0900

    Improve readability.

--- example/main.cu
+++ example/main.cu
@@ -6,19 +6,16 @@ using real = double;
 
 int main()
 {
+	// Mesh and domain settings.
 	const size_t num_cell[3] = { 100, 100, 100 };
 	const size_t NXYZ = num_cell[0] * num_cell[1] * num_cell[2];
-
-	real* ls_cc = new real[NXYZ];
-
-	const char* file_path = "./Stanford_Bunny.stl";
-
 	const real domain_len[3] = { 1, 1, 1 };
-	const real dh[3] = {
-		domain_len[0] / num_cell[0],
-		domain_len[1] / num_cell[1],
-		domain_len[2] / num_cell[2] };
+	const real dh[3] = { domain_len[0] / num_cell[0], domain_len[1] / num_cell[1], domain_len[2] / num_cell[2] };
 
+	// sdf: Signed Distance Function.
+	real* sdf = new real[NXYZ];
+
+	// Coordinate at each cell.
 	real* coord_x = new real[ NXYZ ];
 	real* coord_y = new real[ NXYZ ];
 	real* coord_z = new real[ NXYZ ];
@@ -31,18 +28,24 @@ int main()
 		coord_z[index] = (k + 0.5) * dh[2];
 	}
 
-	const real offset[3] = { 0.3, 0.5, -0.1 };
+	// STL file path.
+	const char* file_path = "./Stanford_Bunny.stl";
 
+	// Parameters for modifying the coordinates of the STL data.
+	// (new coordinate) = (original coordinate) * scale_factor + offset.
 	const real scale_factor = 0.008;
+	const real offset[3] = { 0.3, 0.5, -0.1 };
 
 	sdfGenerator::STL stl;
 	stl.readSTL( file_path, offset, scale_factor );
 	stl.refinePolygon( dh[0] );
-	sdfGenerator::gpu::stl_to_sdf( ls_cc, coord_x, coord_y, coord_z, NXYZ, stl );
+	sdfGenerator::gpu::stl_to_sdf( sdf, coord_x, coord_y, coord_z, NXYZ, stl );
 
-	vtk_write<real>( "bunny.vtk", ls_cc, num_cell, dh );
+	// Output result
+	vtk_write<real>( "bunny.vtk", sdf, num_cell, dh );
 
-	delete[] ls_cc;
+	// Deallocate memeory.
+	delete[] sdf;
 	delete[] coord_x;
 	delete[] coord_y;
 	delete[] coord_z;
